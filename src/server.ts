0cbefc9da62d1bff57585f128cee83d2854a739a
Omkar Patil <omkarpatil290@gmail.com>
2023-03-11 20:00:25 +0530

    add proxy trust to express server

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import nodemailer from "nodemailer";
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const app = express();
+app.set('trust proxy', true);
 
 // Redirect root to Admin panel
 app.get("/", (_, res) => {
